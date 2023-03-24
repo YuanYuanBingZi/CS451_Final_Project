1b250402a0fa043de21fa9f777e33ac723d6e7fe
Tong Guan <96083295+YuanYuanBingZi@users.noreply.github.com>
2023-03-23 23:05:55 -0700

    Update main.php

--- main.php
+++ main.php
@@ -4,9 +4,9 @@
 <html>
 <head>
 	<title>Welcome To Our Pet Store</title>
+	<h1>Welcome To Our Pet Store</h1>
 </head>
 <body>
-	<br>
 	<h2> Please select your role: </h2>
 	<button onclick="window.location.href='customer.php'">Customer</button>
 	<button onclick="window.location.href='staff.php'"> Staff</button>
@@ -14,6 +14,9 @@
 	<footer>
         <div>
 			<a href="implementation_code.txt">Implementation Code</a>
+		</div>
+	 <div>
+			<a href="contents_of_table.html">Contents of Tables</a>
 		</div>
 		<div style="text-align:bottom;">
 			<a href="petstore.txt">Contents of the PHP page</a>
